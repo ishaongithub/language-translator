085323e83d56d655b5dac3689ce5b22941d55630
ishaongithub <75757332+ishaongithub@users.noreply.github.com>
2022-09-01 19:42:30 -0700

    Add files via upload

--- language.py
+++ language.py
@@ -33,7 +33,19 @@ combo2.set("english")
 combo2.place(relx=0.8, rely=0.2, anchor=CENTER)
 Output_text=Text(root, font = 'arial 10', height = 11, wrap = WORD, padx=5, pady=5,  bg="#FFFCF9", bd=0, width=40)
 Output_text.place(relx=0.97, rely=0.5, anchor=E)
-btn=Button(root, text="Translate",font = 'arial 10', bg="blue", fg="white", activebackground="black", relief=FLAT, pady=10)
+btn=Button(root, text="Translate",font = 'arial 10', bg="blue", fg="white", activebackground="black", relief=FLAT, pady=10, command=Translate)
 btn.place(relx=0.5, rely=0.7, anchor=CENTER)
+
+def Translate():
+    obj_trans=Translator()
+    try:
+        translated= obj_trans.translate(Input_text.get(), 0, END, combo.get(), combo2.get())
+        Input_text.delete(0, END)
+        Output_text.insert(END, translated)
+    except:
+        print("Try again")
+        
+label=Label(root, text="Created by Isha", bg="#F2CCC3", font = 'arial 10 bold')
+label.place(relx=0.5, rely=0.99, anchor=CENTER)
 root.mainloop()
 
